62cea62b367d7026515a088ac409ccf22987648f
yeasinarafatprantik2002 <yeasinarafatprantik@gmail.com>
2024-02-16 17:21:57 +0600

    remove

--- vowel_consonant.py
+++
@@ -1,9 +0,0 @@
-letter = input("Enter a letter: ").lower()
-
-if letter.isalpha():
-    if letter in "aeiou":
-        print("Vowel")
-    else:
-        print("Consonant")
-else:
-    print("Invalid input")
